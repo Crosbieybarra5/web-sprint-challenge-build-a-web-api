548577878cff317d393b9433f14fd97d36ffa9dc
Crosbie Ybarra <crosbieybarra5@gmail.com>
2022-05-13 16:13:18 -0500

    fixed

--- api/server.js
+++ api/server.js
@@ -1,9 +1,29 @@
-const express = require('express');
+const express = require("express");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const cors = require("cors");
+const actionRouter = require("./actions/actions-router");
+const projectRouter = require("./projects/projects-router");
+
 const server = express();
 
-// Configure your server here
-// Build your actions router in /api/actions/actions-router.js
-// Build your projects router in /api/projects/projects-router.js
-// Do NOT `server.listen()` inside this file!
+server.use(express.json());
+server.use(helmet());
+server.use(morgan("dev"));
+server.use(cors());
+
+server.use("/api/actions", actionRouter);
+server.use("/api/projects", projectRouter);
+
+server.get("/", (req, res) => {
+  res.send(`<h1>sanity check</h1>`);
+});
+
+server.use((err, req, res, next) => {
+  res.status(err.status || 500).json({
+    message: err.message,
+    stack: err.error.stack,
+  });
+});
 
 module.exports = server;
